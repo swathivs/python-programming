d8022dd506c953361400bab5a1105cd862446b03
swathivs <36623544+swathivs@users.noreply.github.com>
2018-02-20 14:45:12 +0530

    Create mathoperations.py

---
+++ beginner level/mathoperations.py
@@ -0,0 +1,21 @@
+#python version :3 
+a=10
+b=20
+c=30
+d=40
+e=50
+f=60
+g=70
+h=80
+i=90
+j=100
+print("a+b")
+print("c-d")
+print("e*f")
+print("g/h")
+print("i<j")
+print("i>j")
+print("i==j")
+print("id(a)")
+print("a<=d")
+print("a>=b")
